43cf48fe73dd0a32298b461f5023056996f98a04
PalePat <ivan2005ka@gmail.com>
2026-04-16 18:28:36 +0000

    doc: добавление кнопки и поля ввода

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  book_name: string
+  book_author: string
+}
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -11,5 +17,12 @@ export class HomePage {
   constructor() {}
   myname: string = "Ivan Alekseevich"
   mylist: string[] = ["1984", "471 градус по F", "Марадёр", "Патфайндер", "Ищейка", "Пицца", "А места здесь тихие", "Гарри Потер", "Escape From Tarkov", "Пикник на обочине"]
+  myBooks: Book[] = [
+    {book_name: "Война и мир", book_author: "Толстой"},
+    {book_name: "Онегин", book_author: "Автор"}
+  ]
+  mybtn(){
+    this.myname = this.myname + "!"
+  }
 
 }
